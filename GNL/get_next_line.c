6960bc46575ac34456cdb24dddaab8b8a93040b0
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-06-04 14:26:38 +0200

    GNL started

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,11 +6,31 @@
 /*   By: kmarchan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/03 16:02:16 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/06/03 16:43:29 by kmarchan         ###   ########.fr       */
+/*   Updated: 2018/06/04 14:26:05 by kmarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 int		get_next_line(const int fd, char **line)
-
+{
+	int 		i;
+	int 		j;
+	char		*ovr;
+	char buf[BUFF_SIZE + 1];
+	int in;
+	
+	*line = (char*)malloc((BUFF_SIZE *2) * sizeof(char));
+	while (in = read(fd, buf, BUFF_SIZE) > 0)
+	{
+		while (*buf)
+			if()
+				akjudhfakjdhfkljdfh
+					return 
+		if ()
+			jasHGDKHJGFJKHGFHG
+			return;
+	}
+	if (!line)
+		return (-1);
+}
