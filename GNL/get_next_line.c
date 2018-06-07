ab04b8e6e184b97bfd6b4f7ae0a2b117a6221ba3
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-06-07 14:19:19 +0200

    in progress

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -1,138 +1,107 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kmarchan <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/03 16:02:16 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/06/06 13:46:52 by kmarchan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+/*****gnl*******/
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	read_next_buf(size_t in)
+int	get_next_line(const int fd, char **line)
 {
-	if (in > 0)
-		return (1);
-	return (0);
-}
-
-int	ft_srchrep(char *str, char s, char r)
-{
-	int i;
-	int count;
-
-	i = 0;
-	count = 0;
-	if (str && s && r)
-	{
-		while (str[i] != '\0')
-		{
-			if (str[i] == s)
-			{
-				str[i] = r;
-			}	
-			i++;
-		}
-		count++;
-	}		
-	return (count);
-}
-
-
-int		get_next_line(const int fd, char **line)
-{
-	size_t	in;
-	int l;
-//	int i;
-	int lc;
-//	char *ptr;
-	char buf[BUFF_SIZE + 1];
-	static ins	*node;
-
-	l = 0;
-//	r = 0;
+	int		in;
+	char	buf[BUFF_SIZE + 1];
+	char	*ptr;
+	static	ins	*node;
+	
+	ft_bzero(buf, BUFF_SIZE + 1);
+//	printf("%s\n", *line);
+	if (fd < 0 || !line || read(fd, NULL, 0) < 0)
+		return (-1);
+	ft_strclr(*line);
 	if (!node)
 		node = (ins*)ft_memalloc(sizeof(ins));
 	if (!node->hold)
-		node->hold = (char *)ft_memalloc(sizeof(char) * 1000);
+		node->hold = (char *)ft_memalloc(sizeof(char) * 5500000);
+	if (!node->ovr)
+		node->ovr = (char *)ft_memalloc(sizeof(char) * 5500000);
+	if (node->ovr != NULL)
+	{
+		ft_strcat(node->hold, node->ovr);
+		ft_strclr(node->ovr);
+	}
 	while ((in = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-	//	r = read_next_buf(in);
-	//	printf("before %s, ", buf);
-	//	lc = ft_srchrep(buf, '\n', '\0');
-//		i = 0;
-	//	printf("after %s\n, ", buf);
-		while (l <= lc)//i < BUFF_SIZE)
+		buf[in] = '\0';
+		ft_strcat(node->hold, buf);
+		if  (ft_strchr(buf, '\n'))
 		{
-			ptr = ft_memccpy(*line, buf, '\0', BUFF_SIZE);
-			l++;
+			break;
 		}
-		while (l--)
-		{
-			ft_strclr(buf);
-		}
-		/*	printf("%d, %s\n", r,  buf);
-			printf("f");
-			ptr = (ft_memchr(buf, '\n', BUFF_SIZE));
-			printf("f");
-			if ((*ptr) == '\n')
-			{
-				//(ptr - 1) = '\0';
-				printf("f");
-				printf("%s", buf);
-				ft_memccpy(*line, buf, '\n', BUFF_SIZE);
-				l++;
-				ft_strclr(buf);
-				*buf = *ptr + 1;*/
-	//		printf("%s", buf);
-	//		printf("%zu\t, l [%s]\n", in, *line);
-	//	printf("%zu\t, p [%c]\n", in, *ptr);
-			ft_strcat(node->hold, buf);
-			ft_strclr(buf);
-//			printf("%zu\t, h [%s]\n", in,  node->hold);
+		ft_bzero(buf, BUFF_SIZE);
 	}
-	return (1);
+	ptr = NULL;
+	if (ft_strchr(node->hold, '\n'))
+		*(ptr = ft_strchr(node->hold, '\n')) = '\0'; //FUCKING BROKEN
+	*line = ft_strdup(node->hold);
+	ft_strclr(node->hold);	
+	if (ptr)
+		ft_strcat(node->ovr, ptr + 1);				//strchr(node->hold, '\0'));
+	else 
+		ft_strcat(node->ovr, buf);
+//	write(1, "	2	", 1);
+//	printf("%s\n", *line);
+	if (in >= 0 && ft_strlen(*line) > 0)
+		return (1);
+	else 
+	   return (0);	
 }
+/*#include "get_next_line.h"
+#include <stdio.h>
 
-/*
-int main()
-{
-	int i;
-	
-    	// int fd = open("test.txt", O_RDONLY);
-	
-   	 // line = malloc(1000);
- 	 // *line = malloc(1000);
-
-   	 int fd2 = open("test2.txt", O_RDONLY);
-   	 char     **line2;
-   	 // char    **line;
-   	 line2 = malloc(100000);
-   	 *line2 = malloc(100000);
-   	 // int id;
-
-   	 i = get_next_line(fd2, line2);
-  	 printf("%d : len : %zu [%s]\n",i ,ft_strlen(*line2), *line2);	
-}
-*/
+#include <time.h>
+     
 
 int	main()
 {
-	int fd = open("file.txt", O_RDONLY);
-	int i;
-	char **line;
 
-	line = malloc(1000);
-	*line = malloc(1000);	
-	i = get_next_line(fd, line);
-	printf("m%d\t, [%s]\n", i, *line);
-	i = get_next_line(fd, line); 
-	printf("m%d\t, [%s]\n", i, *line);
-	i = get_next_line(fd, line);
-	printf("m%d\t, [%s]\n", i, *line);
+	 clock_t start, end;
+     
+     start = clock();
+	int i;
+	int fd = open("large_file.txt", O_RDONLY);
+	char	**line;
+	line = malloc(100000);
+	*line = malloc(100000);
+	i = 1;
+	int count = 0;
+	while (count != 783)
+	{	
+		count++;
+		i = get_next_line(76678, line);
+		printf("%d : len : %zu [%s]\n",i ,ft_strlen(*line), *line);
+	}
+	 end = clock();
+	printf("%lu\n",end );
+*/
+     // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+/*
+}
+int	main()
+{
+	
+	int i = 0;
+
+	int	fd2 = open("one_big_fat_line.txt", O_RDONLY);
+	char	**line2;
+	line2 = malloc(100000);
+	*line2 = malloc(100000);
+
+	while (i > -1)
+	{
+		i = get_next_line(fd2, line2);
+	}
+//	i = get_next_line(fd2, line2);
+//	printf("%s\n", *line2);
+//	i = get_next_line(fd2, line2);
+//	i = get_next_line(fd2, line2);
+//	i = get_next_line(fd2, line2);
+//	i = get_next_line(fd2, line2);
 	return (0);
+	//printf("%d :len :%u [%s]\n", i, ft(strlen(*line2), *line2);*/
 }
--- GNL/gnl.c
+++ GNL/gnl.c
@@ -4,13 +4,15 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	size_t	in;
+	int		in;
 	char	buf[BUFF_SIZE + 1];
 	char	*ptr;
 	static	ins	*node;
-
+	
 	ft_bzero(buf, BUFF_SIZE + 1);
 //	printf("%s\n", *line);
+	if (fd < 0 || !line || read(fd, NULL, 0) < 0)
+		return (-1);
 	ft_strclr(*line);
 	if (!node)
 		node = (ins*)ft_memalloc(sizeof(ins));
@@ -19,10 +21,13 @@ int	get_next_line(const int fd, char **line)
 	if (!node->ovr)
 		node->ovr = (char *)ft_memalloc(sizeof(char) * 5500000);
 	if (node->ovr != NULL)
+	{
 		ft_strcat(node->hold, node->ovr);
 		ft_strclr(node->ovr);
+	}
 	while ((in = read(fd, buf, BUFF_SIZE)) > 0)
 	{
+		buf[in] = '\0';
 		ft_strcat(node->hold, buf);
 		if  (ft_strchr(buf, '\n'))
 		{
@@ -30,35 +35,73 @@ int	get_next_line(const int fd, char **line)
 		}
 		ft_bzero(buf, BUFF_SIZE);
 	}
-	*(ptr = ft_strchr(node->hold, '\n')) = '\0';
+	ptr = NULL;
+	if (ft_strchr(node->hold, '\n'))
+		*(ptr = ft_strchr(node->hold, '\n')) = '\0'; //FUCKING BROKEN
 	*line = ft_strdup(node->hold);
 	ft_strclr(node->hold);	
-	ft_strcat(node->ovr, ptr + 1);				//strchr(node->hold, '\0'));
-	write(1, "	2	", 1);
-	printf("%s\n", *line);
-	if (in > 0)
-		return (1);
-	if (in == 0)
-		return (0);
+	if (ptr)
+		ft_strcat(node->ovr, ptr + 1);				//strchr(node->hold, '\0'));
 	else 
-	   return (-1);	
+		ft_strcat(node->ovr, buf);
+//	write(1, "	2	", 1);
+//	printf("%s\n", *line);
+	if (in >= 0 && ft_strlen(*line) > 0)
+		return (1);
+	else 
+	   return (0);	
 }
+/*#include "get_next_line.h"
+#include <stdio.h>
+
+#include <time.h>
+     
 
 int	main()
 {
-	int i;
 
-	int	fd2 = open("large_file.txt", O_RDONLY);
+	 clock_t start, end;
+     
+     start = clock();
+	int i;
+	int fd = open("large_file.txt", O_RDONLY);
+	char	**line;
+	line = malloc(100000);
+	*line = malloc(100000);
+	i = 1;
+	int count = 0;
+	while (count != 783)
+	{	
+		count++;
+		i = get_next_line(76678, line);
+		printf("%d : len : %zu [%s]\n",i ,ft_strlen(*line), *line);
+	}
+	 end = clock();
+	printf("%lu\n",end );
+*/
+     // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+/*
+}
+int	main()
+{
+	
+	int i = 0;
+
+	int	fd2 = open("one_big_fat_line.txt", O_RDONLY);
 	char	**line2;
 	line2 = malloc(100000);
 	*line2 = malloc(100000);
 
-	i = get_next_line(fd2, line2);
+	while (i > -1)
+	{
+		i = get_next_line(fd2, line2);
+	}
+//	i = get_next_line(fd2, line2);
 //	printf("%s\n", *line2);
-	i = get_next_line(fd2, line2);
-	i = get_next_line(fd2, line2);
-	i = get_next_line(fd2, line2);
-	i = get_next_line(fd2, line2);
+//	i = get_next_line(fd2, line2);
+//	i = get_next_line(fd2, line2);
+//	i = get_next_line(fd2, line2);
+//	i = get_next_line(fd2, line2);
 	return (0);
-	//printf("%d :len :%u [%s]\n", i, ft(strlen(*line2), *line2);
+	//printf("%d :len :%u [%s]\n", i, ft(strlen(*line2), *line2);*/
 }
