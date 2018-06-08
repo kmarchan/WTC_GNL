d401d5d16e8523e5255f9400dadb8f35fce2fd58
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-06-08 12:42:48 +0200

    stil working on it

--- 42/get_next_line.h
+++ 42/get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: kmarchan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/05 08:19:18 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/06/07 14:30:01 by kmarchan         ###   ########.fr       */
+/*   Updated: 2018/06/08 12:04:50 by kmarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include "libft.h"
+# include "libft/libft.h"
 # include <fcntl.h>
 
-# define BUFF_SIZE 1000000
+# define BUFF_SIZE 1000
 
 typedef struct list
 {
@@ -23,8 +23,9 @@ typedef struct list
 	char	*hold;
 	int		in;
 	int		fd;
-	int		i;
+	int		c;
 }			ins;
 
 int	get_next_line(const int fd, char **line);
+
 #endif
